1d8df5c9928f7c162892b6f747b622de991eeb40
Jorge Rios Sanchez <jorge.rios.sanchez@gmail.com>
2020-03-10 14:04:23 +0100

    segunda version subida. Metodo sumarDobles añadido

--- src/main/java/Suma.java
+++ src/main/java/Suma.java
@@ -3,4 +3,8 @@ public class Suma {
     public int Sumar (int num1, int num2){
         return num1+num2;
     }
+
+    public double Sumar (double num1, double num2){
+        return num1+num2;
+    }
 }
